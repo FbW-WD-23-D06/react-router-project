69a21cb6a69ff00169331abc46c43641e692d05f
rami <rami_zezo1986@hotmail.com>
2024-01-02 10:30:11 +0100

    router component explaining

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,20 @@
-
-
 import './App.css'
+import { Routes , Route} from 'react-router-dom';
+import Home from './pages/Home';
+import About from './pages/About';
+import NotFound from './pages/NotFound';
 
 function App() {
 
   return (
     <>
-     <h1>Hello</h1>
+    
+     <Routes>
+       <Route path='/' element={<Home />}/>
+       <Route path='/about' element={<About />}/>
+       <Route path='*' element={<NotFound />}/>
+     </Routes>
+    
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,23 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import { BrowserRouter} from 'react-router-dom'
+import StaticRouter from 'react-router-dom/server.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
+    {/* Hash Router wird für sharing host benutzt */}
+    {/* <HashRouter> */}
+    {/* Memory Router wird mit testing benutzt */}
+    {/* <MemoryRouter> */}
+    {/* Static router: server side rendering */}
+    {/* <StaticRouter> */}
     <App />
+    {/* </StaticRouter> */}
+    {/* </MemoryRouter> */}
+    {/* </HashRouter> */}
+
+    </BrowserRouter>
   </React.StrictMode>,
 )
